a96d3fe44cff8e9dfeb1a6164cb370b121b9a7af
diego aquino <140066303+diaquinodev@users.noreply.github.com>
2024-03-01 22:27:07 -0300

    Delete FuturoDEV [Trip] - Módulo-1/Semana-4-Exercícios directory

--- FuturoDEV [Trip] - Módulo-1/Semana-4-Exercícios/Ex4funcoescallback.js
+++
@@ -1,9 +0,0 @@
-const adicao = (numero1, numero2, funcaoAnonima) => {
-    const resultado = numero1 + numero2;
-    funcaoAnonima(resultado);
-  };
-  
-  adicao(3, 5, (resultado) => {
-    console.log(`O resultado da adição é: ${resultado}`);
-  });
-  
